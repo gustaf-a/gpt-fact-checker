61c326c112c4562c5cda4236a765b68ed4260599
Gustaf <gustaf.ahlner@gmail.com>
2023-07-05 11:42:28 +0200

    add JsonClient and JsonHelper

---
+++ GptFactChecker/JsonClient/JsonHelper.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+
+namespace GptFactCheckerApi.Repository.JsonRepo;
+
+public static class JsonHelper
+{
+    public static async Task<T> GetObjectFromJson<T>(string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentNullException(nameof(filePath));
+
+        try
+        {
+            string fileAsString = await File.ReadAllTextAsync(filePath);
+
+            if (string.IsNullOrWhiteSpace(fileAsString))
+                return default;
+
+            var obj = Deserialize<T>(fileAsString);
+
+            return obj ?? default;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+            return default;
+        }
+    }
+
+    public static async Task SaveToJson(object objectToSave, string filePath)
+    {
+        if (string.IsNullOrWhiteSpace(filePath))
+            throw new ArgumentNullException(nameof(filePath));
+
+        var claimClaimChecksJson = Serialize(objectToSave);
+
+        await File.WriteAllTextAsync(filePath, claimClaimChecksJson);
+    }
+
+    public static string Serialize(object ojectToSerialize, bool includeNullValues = true)
+    {
+        JsonSerializerSettings settings = new()
+        {
+            NullValueHandling = includeNullValues ? NullValueHandling.Include : NullValueHandling.Ignore
+        };
+
+        return JsonConvert.SerializeObject(ojectToSerialize, settings);
+    }
+
+    public static T Deserialize<T>(string jsonString)
+    {
+        return JsonConvert.DeserializeObject<T>(jsonString);
+    }
+}
