b1654c6a78e5022c2c8d951a58df2f025b5e2dfa
Gustaf <gustaf.ahlner@gmail.com>
2023-08-08 10:13:38 +0200

    Update SourceDto with nullable fields

--- GptFactChecker/GptFactCheckerApiController/Model/SourceDto.cs
+++ GptFactChecker/GptFactCheckerApiController/Model/SourceDto.cs
@@ -3,24 +3,24 @@
 public class SourceDto
 {
     public string Id { get; set; }
-    public string Name { get; set; }
-    public string Language { get; set; }
-    public string Description { get; set; }
-    public List<string> Tags { get; set; }
+    public string Name { get; set; } = "";
+    public string? SourceUrl { get; set; } = "";
+    public string? Language { get; set; } = "";
+    public string? Description { get; set; } = "";
+    public List<string>? Tags { get; set; } = new List<string>();
 
-    public string SourceType { get; set; }
-    public string SourcePerson { get; set; }
-    public string SourceContext { get; set; }
-    public string SourceUrl { get; set; }
-    public string SourceRawText { get; set; }
+    public string? SourceType { get; set; } = "";
+    public string? SourcePerson { get; set; } = "";
+    public string? SourceContext { get; set; } = "";
+    public string? SourceRawText { get; set; } = "";
 
-    public string SourceImportedDate { get; set; }
-    public string SourceCreatedDate { get; set; }
+    public string? SourceImportedDate { get; set; } = "";
+    public string? SourceCreatedDate { get; set; } = "";
 
-    public string ClaimsFirstExtractedDate { get; set; }
-    public string ClaimsUpdatedDate { get; set; }
+    public string? ClaimsFirstExtractedDate { get; set; } = "";
+    public string? ClaimsUpdatedDate { get; set; } = "";
 
-    public string CoverImageUrl { get; set; }
+    public string? CoverImageUrl { get; set; } = "";
 
     public List<ClaimDto>? Claims { get; set; }
 }
