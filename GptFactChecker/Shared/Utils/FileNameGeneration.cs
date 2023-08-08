615a6c8ed69ededb6bf1a0aa81d5a517ac493ee9
Gustaf <gustaf.ahlner@gmail.com>
2023-08-08 12:19:19 +0200

    add FileNameGeneration util in shared

---
+++ GptFactChecker/Shared/Utils/FileNameGeneration.cs
@@ -0,0 +1,52 @@
+﻿using System.Text;
+
+namespace Shared.Utils;
+
+public static class FileNameGeneration
+{
+    /// <summary>
+    /// Creates a valid filename by removing any invalid characters from the input string.
+    /// </summary>
+    /// <param name="fileName">The string to transform into a valid filename.</param>
+    /// <returns>A valid filename string.</returns>
+    public static string CreateFileNameFromString(string fileName)
+    {
+        if (fileName == null)
+            throw new ArgumentNullException(nameof(fileName));
+
+        var invalidChars = Path.GetInvalidFileNameChars();
+
+        var validFileName = new StringBuilder();
+
+        foreach (var ch in fileName)
+            if (!invalidChars.Contains(ch))
+                validFileName.Append(ch);
+
+        var result = validFileName.ToString();
+
+        result = result.Replace(" ", "");
+
+        if (result.Length > 40)
+            result = result.Substring(0, 40);
+
+        if (string.IsNullOrWhiteSpace(result))
+            result = IdGeneration.GenerateStringId();
+
+        return result;
+    }
+
+    public static string EnsureFilePathIsUnique(string filePath)
+    {
+        if (!File.Exists(filePath))
+            return filePath;
+
+        string directory = Path.GetDirectoryName(filePath);
+        string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(filePath);
+        string extension = Path.GetExtension(filePath);
+
+        string newFileName = $"{fileNameWithoutExtension}_{IdGeneration.GenerateStringId()}{extension}";
+
+        return Path.Combine(directory, newFileName);
+    }
+}
+
---
+++ GptFactChecker/SharedTests/Utils/FileNameGenerationTests.cs
@@ -0,0 +1,56 @@
+﻿using Shared.Utils;
+
+namespace SharedTests.Utils;
+
+public class FileNameGenerationTests
+{
+    [Fact]
+    public void CreateFileNameFromString_ShouldThrowArgumentNullException_WhenNullIsPassed()
+    {
+        Assert.Throws<ArgumentNullException>(() => FileNameGeneration.CreateFileNameFromString(null));
+    }
+
+    [Fact]
+    public void CreateFileNameFromString_ShouldRemoveInvalidChars_FromFileName()
+    {
+        string fileName = "test<>|\"?:*file.txt";
+        string expected = "testfile.txt";
+
+        string actual = FileNameGeneration.CreateFileNameFromString(fileName);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CreateFileNameFromString_ShouldNotChange_ValidFileName()
+    {
+        string fileName = "validfile.txt";
+        string expected = "validfile.txt";
+
+        string actual = FileNameGeneration.CreateFileNameFromString(fileName);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CreateFileNameFromString_ShouldTrimSpaces()
+    {
+        string fileName = "valid file ";
+        string expected = "validfile";
+
+        string actual = FileNameGeneration.CreateFileNameFromString(fileName);
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void CreateFileNameFromString_ShouldShortenFileName_WhenFileNameIsTooLong()
+    {
+        string fileName = new string('a', 50);
+        string expected = new string('a', 40);
+
+        string actual = FileNameGeneration.CreateFileNameFromString(fileName);
+
+        Assert.Equal(expected, actual);
+    }
+}
