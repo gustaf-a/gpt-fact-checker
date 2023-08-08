f3388964b2212c6b6a1b98d88190bcd56f13a589
Gustaf <gustaf.ahlner@gmail.com>
2023-08-08 13:14:28 +0200

    Update Topic model

--- GptFactChecker/Shared/Models/Topic.cs
+++ GptFactChecker/Shared/Models/Topic.cs
@@ -1,11 +1,20 @@
 ﻿namespace Shared.Models;
 
-public class Topic : IIdentifiable
+public class Topic : IIdentifiable, IComparable<Topic>
 {
     public string Id { get; set; } = "";
     public string Name { get; set; } = "";
     public string Description { get; set; }
     public List<string> Tags { get; set; } = new();
+    public int Priority { get; set; } = 50;
 
     public List<Reference>? References { get; set; } = null;
+
+    public int CompareTo(Topic? other)
+    {
+        if(other is null) 
+            return -1; 
+
+        return Priority.CompareTo(other.Priority);
+    }
 }
