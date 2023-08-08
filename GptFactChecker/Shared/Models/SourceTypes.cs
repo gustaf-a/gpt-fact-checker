5660793fb9ccb01249d33b85b8fe30a844b96e31
Gustaf <gustaf.ahlner@gmail.com>
2023-08-08 13:10:14 +0200

    Create SourceTypes.cs

---
+++ GptFactChecker/Shared/Models/SourceTypes.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Shared.Models;
+
+public static class SourceTypes
+{
+    public static List<string> AllTypes = new()
+    {
+        Video, Podcast, Text, SocialMedia
+    };
+
+    public const string Video = "video";
+    public const string Podcast = "podcast";
+    public const string Text = "text";
+    public const string SocialMedia = "socialmedia";
+}
