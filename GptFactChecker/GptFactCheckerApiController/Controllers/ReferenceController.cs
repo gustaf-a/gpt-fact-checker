69138271588b99bf2f555f72ca771906ad30d7a4
Gustaf <gustaf.ahlner@gmail.com>
2023-08-08 13:14:36 +0200

    Add ReferenceController

--- GptFactChecker/GptFactCheckerApiController/Controllers/ReferenceController.cs
+++ GptFactChecker/GptFactCheckerApiController/Controllers/ReferenceController.cs
@@ -17,6 +17,20 @@ public class ReferenceController : ControllerBase
     }
 
     /// <summary>
+    /// Creates references related to a topic
+    /// </summary>
+    [HttpPost("topic/id/batch")]
+    public async Task<IActionResult> CreateReferenceForTopic([FromQuery] string topicId, [FromBody] List<Reference> references)
+    {
+        var response = await _referenceService.CreateReferences(references, topicId);
+
+        if (!response)
+            return StatusCode(500);
+
+        return Ok();
+    }
+
+    /// <summary>
     /// Creates a reference related to a topic
     /// </summary>
     [HttpPost("topic/id")]
@@ -28,6 +42,51 @@ public class ReferenceController : ControllerBase
             return StatusCode(500);
 
         return Ok();
+    }
+
+    /// <summary>
+    /// Returns all topics as an unsorted JSON collection
+    /// </summary>
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var references = await _referenceService.GetAll();
+
+        return Ok(references);
+    }
+
+    /// <summary>
+    /// Returns a reference matching the provided id
+    /// </summary>
+    [HttpGet("id")]
+    public async Task<IActionResult> GetById([FromQuery] string id)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+            return NotFound();
+
+        var reference = await _referenceService.GetById(id);
+
+        if (reference is null)
+            return NotFound();
+
+        return Ok(reference);
+    }
+
+    /// <summary>
+    /// Returns a reference matching the provided id
+    /// </summary>
+    [HttpGet("topic/id")]
+    public async Task<IActionResult> GetByTopicId([FromQuery] string id)
+    {
+        if (string.IsNullOrWhiteSpace(id))
+            return NotFound();
+
+        var reference = await _referenceService.GetReferencesByTopic(id);
+
+        if (reference is null)
+            return NotFound();
+
+        return Ok(reference);
     }
 
     /// <summary>
